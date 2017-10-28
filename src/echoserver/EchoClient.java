864ead0720f16ca12ef9258618dac1157d1fa53d
carav008 <carav008@morris.umn.edu>
2017-10-28 00:40:02 -0500

    Finish lab. All tests pass.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,31 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
+
 
 public class EchoClient {
+  public static void main(String[] args) {
+    try {
+      Socket socket = new Socket("127.0.0.1", 6013);
+      // You can change 127.0.0.1 to a machine name if you want to try this across
+      // the network to another machine.
+      // Socket socket = new Socket("some.machine.edu", 6013);
+      InputStream in = new DataInputStream(System.in); //Reads data from the standart input
+      OutputStream out = socket.getOutputStream(); //OutputStream to send bytes to server
+      InputStream input = socket.getInputStream(); //InputStream to receive bytes from server
+      byte[] inBuffer = new byte[1]; //Buffer with size one so we can read byte by byte
+      byte[] outBuffer = new byte[1]; //Buffer with size one so we can receive byte by byte
+
+      while(in.read(inBuffer)!=-1){ //Once we reach end of the standart input we stop
+        out.write(inBuffer); //Sends byte to the server
+        out.flush();
+        input.read(outBuffer); //Receives the byte from the server
+        System.out.write(outBuffer, 0, 1); //Prints out this byte
+      }
+      socket.close();
+    } catch (IOException ioe) {
+      System.out.println("We caught an exception");
+      System.err.println(ioe);
+    }
+  }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,27 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoServer {
+  public static void main(String[] args) {
+    try {
+      ServerSocket sock = new ServerSocket(6013);
+
+      while (true) {
+        System.out.println("Got a request!");
+        Socket client = sock.accept();
+        InputStream input  = client.getInputStream(); //InputStream to receive the bytes from client
+        OutputStream output = client.getOutputStream(); //OutputStream to send bytes to client
+        byte[] buffer = new byte[1]; //Buffer with size one so we can read byte by byte
+
+        while(input.read(buffer)!=-1){
+          output.write(buffer); //Sends the same byte back to the client
+          output.flush();
+        }
+        client.close();
+      }
+    } catch (IOException ioe) {
+      System.err.println(ioe);
+    }
+  }
 }
